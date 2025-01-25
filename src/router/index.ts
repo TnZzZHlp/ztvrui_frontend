4b5fb1a8bac86bc576f863ca7e1a466ebfaacf5a
TnZzZHlp <tnzzzhlp@outlook.com>
2025-01-25 20:31:47 +0800

    refactor: Lazy load components in router for improved performance

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import HomeView from '../views/HomeView.vue'
+// import LoginView from '../views/LoginView.vue'
+// import HomeView from '../views/HomeView.vue'
 
-import NetworksComponent from '@/components/HomeComponents/Networks.vue'
-import NetworkComponent from '@/components/HomeComponents/Network.vue'
-import UserManageComponent from '@/components/HomeComponents/UserManage.vue'
+// import NetworksComponent from '@/components/HomeComponents/Networks.vue'
+// import NetworkComponent from '@/components/HomeComponents/Network.vue'
+// import UserManageComponent from '@/components/HomeComponents/UserManage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'login',
-      component: LoginView,
+      component: () => import('@/views/LoginView.vue'),
     },
     {
       path: '/home',
       name: 'home',
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue'),
       children: [
         {
           path: '',
           name: 'networks',
-          component: NetworksComponent,
+          component: () => import('@/components/HomeComponents/Networks.vue'),
         },
         {
           path: 'network/:id',
           name: 'network',
-          component: NetworkComponent,
+          component: () => import('@/components/HomeComponents/Network.vue'),
         },
         {
           path: 'user_manage',
           name: 'user_manage',
-          component: UserManageComponent,
+          component: () => import('@/components/HomeComponents/UserManage.vue'),
+        },
+        {
+          path: '*',
+          redirect: '/',
         },
       ],
     },
